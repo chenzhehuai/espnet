1b0db6677ccb7706e14126796db5b3b9673aa6d9
Zhehuai Chen <chenzhehuai@foxmail.com>
2018-09-25 13:06:44 -0700

    fix the bug that deeptext miss some words in the input

--- egs/librispeech/asr1/local/tagger_post.py
+++ egs/librispeech/asr1/local/tagger_post.py
@@ -5,13 +5,19 @@ for line in sys.stdin:
   la=line.split("\t")
   if len(la)<3:
     continue
+  snt0=la[0].split()
   snt_js=eval(la[1])
   lab_js=eval(la[3])
   out=''
-  for i in range(len(snt_js)):
+  i=0
+  for j in range(len(snt0)):
+    if i>=len(snt_js) or snt0[j]!=snt_js[i]: # we do this because deeptext will miss some words
+      out+=snt0[j]+" "
+      continue
     if lab_js[i].find("NoLab") == -1:
       lab=lab_js[i].split(":")[1]
       out+="<"+lab+"> "+snt_js[i]+" </"+lab+">"+" "
     else:
       out+=snt_js[i]+" "
+    i+=1
   print out
